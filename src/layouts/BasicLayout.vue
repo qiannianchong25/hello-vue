85254a72c363144b489c1a16e074ce738e05d15c
qiannianchong25 <qiannianchong25@163.com>
2021-06-19 19:02:54 +0800

    home-vue:初次提交

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,82 @@
+<template>
+  <a-layout id="components-layout-demo-custom-trigger">
+    <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+        <a-menu-item key="1">
+          <a-icon type="user" />
+          <span>nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="video-camera" />
+          <span>nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="upload" />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+          class="trigger"
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="() => (collapsed = !collapsed)"
+        />
+      </a-layout-header>
+      <a-layout-content
+        :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+      >
+        Content
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script>
+import {Layout,Menu,Icon} from 'ant-design-vue'
+import Header from './Header'
+import Footer from './Footer'
+import SideMenu from './SideMenu'
+export default {
+   components:{
+       [Layout.name]:Layout,
+       [Layout.Header.name]:Layout.Header,
+       [Layout.Footer.name]:Layout.Footer,
+       [Layout.Sider.name]:Layout.Sider,
+       [Layout.Content.name]:Layout.Content,
+       [Menu.name]:Menu,
+       [Menu.Item.name]:Menu.Item,
+       [Icon.name]:Icon,
+       Header,
+       Footer,
+       SideMenu
+   },
+   data() {
+    return {
+      collapsed: false,
+    };
+  },
+}
+</script>
+
+<style scoped>
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+</style>
